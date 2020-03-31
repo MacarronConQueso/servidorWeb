bd402b1a7b266d2a6043760e15f34771b8743ce2
Jose Moreno Calderón <botocone30@gmail.com>
2020-03-31 10:39:25 +0200

    Entrega de prueba

--- servidor/productes.js
+++ servidor/productes.js
@@ -30,11 +30,17 @@ class Obj {
             try {
                 sql = 'CREATE TABLE productes (id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY, nom VARCHAR(50) NOT NULL, descripcio TEXT, preu INT(6), imatge VARCHAR(255))'
                 await db.promiseQuery(sql)
-                sql = 'INSERT INTO productes (nom, descripcio, preu, imatge) VALUES ("Tele", "Tele molt xula", 800, "/web/imatges/producte-1.jpg")'
+                sql = 'INSERT INTO productes (nom, descripcio, preu, imatge) VALUES ("Nova York", "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut mattis sed erat vulputate eleifend. Etiam a enim nulla. Aliquam vulputate nisl in odio fermentum placerat. Interdum et malesuada fames ac ante ipsum primis in faucibus. Donec iaculis sem malesuada ipsum aliquet imperdiet. Vivamus sem dolor, porttitor eu hendrerit sit amet, consectetur eu dolor. Vivamus sit amet laoreet massa. Suspendisse potenti. Duis ac nisl mollis, pretium odio ac, sodales sapien. Nam dolor velit, lobortis in mattis vitae, commodo consectetur risus.", 850, "/web/imatges/nuevayork.jpg")'
                 await db.promiseQuery(sql)
-                sql = 'INSERT INTO productes (nom, descripcio, preu, imatge) VALUES ("Batidora", "Batidora molt xula", 20, "/web/imatges/producte-2.png")'
+                sql = 'INSERT INTO productes (nom, descripcio, preu, imatge) VALUES ("Roma", "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut mattis sed erat vulputate eleifend. Etiam a enim nulla. Aliquam vulputate nisl in odio fermentum placerat. Interdum et malesuada fames ac ante ipsum primis in faucibus. Donec iaculis sem malesuada ipsum aliquet imperdiet. Vivamus sem dolor, porttitor eu hendrerit sit amet, consectetur eu dolor. Vivamus sit amet laoreet massa. Suspendisse potenti. Duis ac nisl mollis, pretium odio ac, sodales sapien. Nam dolor velit, lobortis in mattis vitae, commodo consectetur risus.", 550, "/web/imatges/Roma-bridge-river-city-sunset_3840x2160.jpg")'
                 await db.promiseQuery(sql)
-                sql = 'INSERT INTO productes (nom, descripcio, preu, imatge) VALUES ("Aspirador", "Aspirador molt xulo", 300, "/web/imatges/producte-3.jpg")'
+                sql = 'INSERT INTO productes (nom, descripcio, preu, imatge) VALUES ("México", "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut mattis sed erat vulputate eleifend. Etiam a enim nulla. Aliquam vulputate nisl in odio fermentum placerat. Interdum et malesuada fames ac ante ipsum primis in faucibus. Donec iaculis sem malesuada ipsum aliquet imperdiet. Vivamus sem dolor, porttitor eu hendrerit sit amet, consectetur eu dolor. Vivamus sit amet laoreet massa. Suspendisse potenti. Duis ac nisl mollis, pretium odio ac, sodales sapien. Nam dolor velit, lobortis in mattis vitae, commodo consectetur risus.", 720, "/web/imatges/cancun-en-mexico-4605.jpg")'
+                await db.promiseQuery(sql)
+                sql = 'INSERT INTO productes (nom, descripcio, preu, imatge) VALUES ("Mallorca", "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut mattis sed erat vulputate eleifend. Etiam a enim nulla. Aliquam vulputate nisl in odio fermentum placerat. Interdum et malesuada fames ac ante ipsum primis in faucibus. Donec iaculis sem malesuada ipsum aliquet imperdiet. Vivamus sem dolor, porttitor eu hendrerit sit amet, consectetur eu dolor. Vivamus sit amet laoreet massa. Suspendisse potenti. Duis ac nisl mollis, pretium odio ac, sodales sapien. Nam dolor velit, lobortis in mattis vitae, commodo consectetur risus.", 400, "/web/imatges/mallorca.jpg")'
+                await db.promiseQuery(sql)
+                sql = 'INSERT INTO productes (nom, descripcio, preu, imatge) VALUES ("Praga", "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut mattis sed erat vulputate eleifend. Etiam a enim nulla. Aliquam vulputate nisl in odio fermentum placerat. Interdum et malesuada fames ac ante ipsum primis in faucibus. Donec iaculis sem malesuada ipsum aliquet imperdiet. Vivamus sem dolor, porttitor eu hendrerit sit amet, consectetur eu dolor. Vivamus sit amet laoreet massa. Suspendisse potenti. Duis ac nisl mollis, pretium odio ac, sodales sapien. Nam dolor velit, lobortis in mattis vitae, commodo consectetur risus.", 540, "/web/imatges/praga.jpg")'
+                await db.promiseQuery(sql)
+                sql = 'INSERT INTO productes (nom, descripcio, preu, imatge) VALUES ("London", "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut mattis sed erat vulputate eleifend. Etiam a enim nulla. Aliquam vulputate nisl in odio fermentum placerat. Interdum et malesuada fames ac ante ipsum primis in faucibus. Donec iaculis sem malesuada ipsum aliquet imperdiet. Vivamus sem dolor, porttitor eu hendrerit sit amet, consectetur eu dolor. Vivamus sit amet laoreet massa. Suspendisse potenti. Duis ac nisl mollis, pretium odio ac, sodales sapien. Nam dolor velit, lobortis in mattis vitae, commodo consectetur risus.", 600, "/web/imatges/londres.jpg")'
                 await db.promiseQuery(sql)
             } catch (e) {
                 console.error(e)
@@ -51,7 +57,7 @@ class Obj {
             return result.json({ resultat: "ko", missatge: "Error, funció llistatProductes: ha fallat la crida a les dades"})  
         }   
     
-        // Si hem aconseguit dades corectament, tornem la taula resultant
+        // Si l'usuari existeix i s'ha identificat correctament (amb codi o amb token) retornem 'ok'
         if (typeof taula === 'object' && typeof taula.length === 'number') {
             result.json({ resultat: "ok", missatge: taula })
         } else {
--- web/script.js
+++ web/script.js
@@ -3,6 +3,8 @@ var navegacio = null
 var popups = null
 var seccioBackofficeUsuaris = null
 var seccioFrontendProductes = null
+var menuVisibleMobil = false
+var i = 0
 
 // Aquesta funció s'inicia al carregar la pàgina
 async function inicia () {
@@ -38,3 +40,51 @@ function iniciaSeccio(seccio) {
     }
 }
 
+
+
+function mostraMenu (evt) {
+    let refBody = document.getElementsByTagName('body')[0],
+        refSmall = document.getElementById('frontendMenuMobile'),
+        refContainer = document.getElementById('menuContainer'),
+        estilSmall = window.getComputedStyle(refSmall, ''),
+        estilContainer = window.getComputedStyle(refContainer, ''),
+        midaContainer = parseInt(estilContainer.getPropertyValue('height')),
+        altura = - midaContainer + 10
+
+    refBody.style.overflow = 'hidden' // Treure scroll
+    refSmall.style.visibility = 'visible'
+    refSmall.style.opacity = 1
+
+    refContainer.style.transform =  'translateY(' + altura + 'px)'
+}
+function amagaMenu (evt) {
+    let refBody = document.getElementsByTagName('body')[0],
+        refSmall = document.getElementById('frontendMenuMobile'),
+        refContainer = document.getElementById('menuContainer')
+
+    refBody.style.overflow = 'auto' // Recuperar scroll
+
+    refSmall.style.opacity = 0
+    setTimeout(() => { refSmall.style.visibility = 'hidden' }, 500)
+
+    refContainer.style.transform = 'translateY(0)'
+}
+
+/*Barra de carga*/
+function carga() {
+    if (i == 0) {
+        i = 1;
+        var elem = document.getElementById(barra);
+        var width =1;
+        var id = setInterval(frame,10);
+        function frame() {
+            if (width >= 100) {
+                clearInterval(id);
+                i = 0;
+            } else {
+                width++;
+                elem.style.width = width + "%";
+            }
+        }
+    }
+}
