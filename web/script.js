474ea2da43203552e16bc8b056bbb92b6d125f6e
Jose Moreno Calderón <botocone30@gmail.com>
2020-04-12 18:15:45 +0200

    Canvis en l'inici de la pàgina, falten fer moltes coses més

--- web/script.js
+++ web/script.js
@@ -88,3 +88,11 @@ function carga() {
         }
     }
 }
+
+/*Carousel*/
+'#carouselExampleIndicators'.on('slid.bs.carousel'), function (e) {
+    'carouselExampleIndicators'.carousel('2') // Will slide to the slide 2 as soon as the transition to slide 1 is finished
+  }
+  
+  '#carouselExampleIndicators'.carousel('1') // Will start sliding to the slide 1 and returns to the caller
+  '#carouselExampleIndicators'.carousel('2') // !! Will be ignored, as the transition to the slide 1 is not finished !!
